b06b0629c9a185a91476d8684cacd3a4fcb3f127
cmoussa1 <moussa1@llnl.gov>
2018-06-26 09:38:34 -0700

    Added sort command line argument.

--- cla_hostlist.py
+++ cla_hostlist.py
@@ -13,26 +13,21 @@ Date: June 26, 2018
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-e", "--expand", dest="expand", help="Expand a compressed hostlist",
-	action="store")
+parser.add_argument("-e", "--expand", dest="expand", help="Expand a compressed hostlist")
 
-parser.add_argument("-a", "--abbreviate", dest="compress_range", help="Compress an expanded hostlist",
-	action="store")
+parser.add_argument("-a", "--abbreviate", dest="compress_range", help="Compress an expanded hostlist")
 
-parser.add_argument("-t", "--tighten", dest="compress", help="Return a hostlist string",
-	action="store")
+parser.add_argument("-t", "--tighten", dest="compress", help="Return a hostlist string")
 
-parser.add_argument("-m", "--minus", dest="diff", nargs="2", help="Subtract all HOSTLIST args from first HOSTLIST",
-	action="store")
+parser.add_argument("-m", "--minus", dest="diff", nargs="*", help="Subtract all HOSTLIST args from first HOSTLIST")
 
-parser.add_argument("-i", "--intersection", dest="intersection", nargs="*", help="Intersection of all HOSTLIST args",
-	action="store")
+parser.add_argument("-i", "--intersection", dest="intersection", nargs="*", help="Intersection of all HOSTLIST args")
 
-parser.add_argument("-u", "--union", dest="union", nargs="*", help="Union of all HOSTLIST arguments",
-	action="store")
+parser.add_argument("-u", "--union", dest="union", nargs="*", help="Union of all HOSTLIST arguments")
 
-parser.add_argument("-n", "--nth=N", dest="nth", nargs="*", help="Output the host at index N",
-	action="store")
+parser.add_argument("-n", "--nth=N", dest="nth", nargs="*", help="Output the host at index N")
+
+parser.add_argument("-s", "--sort", dest="sort", help="Return sorted HOSTLIST")
 
 args = parser.parse_args()
 
@@ -50,3 +45,5 @@ if args.union:
 	hl.union_nodes(args.union[0], args.union[1])
 if args.nth:
 	hl.nth(args.nth[0], args.nth[1])
+if args.sort:
+	hl.sort_nodes(args.sort)
