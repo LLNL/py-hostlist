5de5da245c1baf2802fab846c263ee799ad56689
cmoussa1 <moussa1@llnl.gov>
2018-06-26 11:40:46 -0700

    Fixed help message for command line arguments

--- cla_hostlist.py
+++ cla_hostlist.py
@@ -11,27 +11,47 @@ Date: June 26, 2018
 """
 
 
-parser = argparse.ArgumentParser()
+def msg(name=None):
+	return '''
+	Description: cla_hostlist processes slurm-style hostlist strings and 
+	can return those strings in manipulated fashion. 
+	
+	Usage: python cla_hostlist.py [OPTION]... [HOSTLIST]...
 
-parser.add_argument("-e", "--expand", dest="expand", help="Expand a compressed hostlist")
+  	-h, --help                   Display this message.
+  	-e, --expand                 Expand a compressed hostlist
+  	-a, --abbreviate             Compress an expanded hostlist
+  	-t, --tighten                Return a hostlist string
+  	-m, --minus                  Subtract all HOSTLIST args from first HOSTLIST
+  	-i, --intersection           Intersection of all HOSTLIST args
+  	-u, --union                  Union of all HOSTLIST arguments
+  	-n, --nth=N                  Output the host at index N
+  	-S, --sort                   Return sorted HOSTLIST 
+  	-c, --count                  Print the number of hosts
+  	-F, --find=HOST              Output position of HOST in result HOSTLIST
+		'''
 
-parser.add_argument("-a", "--abbreviate", dest="compress_range", help="Compress an expanded hostlist")
+parser = argparse.ArgumentParser(usage=msg())
 
-parser.add_argument("-t", "--tighten", dest="compress", help="Return a hostlist string")
+parser.add_argument("-e", "--expand", dest="expand")
 
-parser.add_argument("-m", "--minus", dest="diff", nargs="*", help="Subtract all HOSTLIST args from first HOSTLIST")
+parser.add_argument("-a", "--abbreviate", dest="compress_range")
 
-parser.add_argument("-i", "--intersection", dest="intersection", nargs="*", help="Intersection of all HOSTLIST args")
+parser.add_argument("-t", "--tighten", dest="compress")
 
-parser.add_argument("-u", "--union", dest="union", nargs="*", help="Union of all HOSTLIST arguments")
+parser.add_argument("-m", "--minus", dest="diff", nargs="*")
 
-parser.add_argument("-n", "--nth=N", dest="nth", nargs="*", help="Output the host at index N")
+parser.add_argument("-i", "--intersection", dest="intersection", nargs="*")
 
-parser.add_argument("-s", "--sort", dest="sort", help="Return sorted HOSTLIST")
+parser.add_argument("-u", "--union", dest="union", nargs="*")
 
-parser.add_argument("-c", "--count", dest="count", help="Print the number of hosts")
+parser.add_argument("-n", "--nth=N", dest="nth", nargs="*")
 
-parser.add_argument("-f", "--find", dest="find", nargs="*", help="Output position of HOST in result HOSTLIST")
+parser.add_argument("-s", "--sort", dest="sort")
+
+parser.add_argument("-c", "--count", dest="count")
+
+parser.add_argument("-f", "--find", dest="find", nargs="*")
 
 args = parser.parse_args()
 
