495283377d73aef5bef5440aa89487c4de8a1445
cmoussa1 <moussa1@llnl.gov>
2018-07-10 08:56:29 -0700

    Added extension to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../py-hostlist/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -39,8 +39,7 @@ release = u'1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
