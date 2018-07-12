cd9abe070aee6f8befac55a405389b77bc8116e7
cmoussa1 <moussa1@llnl.gov>
2018-07-12 10:49:19 -0700

    Changed syspath

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../py-hostlist/hostlist.py'))
 
 
 # -- Project information -----------------------------------------------------
